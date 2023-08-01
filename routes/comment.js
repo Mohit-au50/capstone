66b8beaf52b77f575b3b1af82c50c679a15aba1b
sanjay <111633827+Sanjay-acharya-au50@users.noreply.github.com>
2023-08-01 13:17:26 +0530

    Update comment.js

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,8 @@ const commentRouter = require('express').Router()
 // get all comments from post
 commentRouter.get('/:postId', getCommentsFromPost)
 
+
+
 // create a comment
 commentRouter.post('/', verifyToken, createComment)
 
